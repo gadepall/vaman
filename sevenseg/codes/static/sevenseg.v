388ec7ebb62fc86a59cacdba9ac25d90045d5192
gadepall <gadepall@gmail.com>
2021-01-18 19:01:13 +0530

    Add existing files

--- sevenseg/codes/static/sevenseg.v
+++ sevenseg/codes/static/sevenseg.v
@@ -1,14 +1,11 @@
 /*Program for driving a seven segment display
-January 17,2021
+January 18,2021
 Code by G V V Sharma
 Released under GNU GPL
 */
 
 //declaring the blink module
 module helloworldfpga(
-    output wire redled,
-    output wire greenled,
-    output wire blueled,
                  output a,
                  output b,
                  output c,
@@ -18,38 +15,14 @@ module helloworldfpga(
                output g
 
 );
-    wire clk;
 
-    qlal4s3b_cell_macro u_qlal4s3b_cell_macro (
-        .Sys_Clk0 (clk),
-    );
-
-    reg[26:0] delay;
-     reg	led;
-
-
-always@(posedge clk) begin
-delay = delay+1; //incrementing the counter.
-//counts from 0 to 20000000 in 1 second
-
-if(delay > 20000000) //blink delay in decimal
-begin
-delay=27'b0;
-led=0; //keep led on
-end
-end
-
-//    assign blueled = led;	//If you want to change led colour to blue, 
-//assign redled = led; //If you want to change led colour to red, 
-assign greenled = !led; //If you want to change led colour to green, 
-assign     a=led;
-assign     b=!led;
-assign     c=led;
-assign     d=led;
-assign     e=!led;
-assign     f=led;
-assign     g=led;
-
+assign     a=0;
+assign     b=1;
+assign     c=0;
+assign     d=0;
+assign     e=1;
+assign     f=0;
+assign     g=0;
 endmodule
 //end of the module
 
