1dce9d03fe015ae536630f07d36c768997fc792d
gadepall <gadepall@gmail.com>
2021-01-27 06:44:27 +0530

    Add existing files

---
+++ arm/codes/setup/red/GCC_Project/output/depend/crc32.d
@@ -0,0 +1,2 @@
+/storage/emulated/0/github/vaman/arm/codes/blink/GCC_Project/output/crc32.o: \
+ /root/pygmy-dev/pygmy-sdk/Libraries/Utils/src/crc32.c
---
+++ arm/codes/setup/red/GCC_Project/output/depend/eoss3_hal_adc.d
@@ -0,0 +1,68 @@
+/storage/emulated/0/github/vaman/arm/codes/blink/GCC_Project/output/eoss3_hal_adc.o: \
+ /root/pygmy-dev/pygmy-sdk/HAL/src/eoss3_hal_adc.c \
+ /storage/emulated/0/github/vaman/arm/codes/blink/inc/Fw_global_config.h \
+ /root/pygmy-dev/pygmy-sdk/HAL/inc/eoss3_dev.h \
+ /root/pygmy-dev/pygmy-sdk/Libraries/CMSIS/inc/core_cm4.h \
+ /root/pygmy-dev/pygmy-sdk/Libraries/CMSIS/inc/core_cmInstr.h \
+ /root/pygmy-dev/pygmy-sdk/Libraries/CMSIS/inc/core_cmFunc.h \
+ /root/pygmy-dev/pygmy-sdk/Libraries/CMSIS/inc/core_cm4_simd.h \
+ /root/pygmy-dev/pygmy-sdk/HAL/inc/eoss3_hal_adc.h \
+ /root/pygmy-dev/pygmy-sdk/HAL/inc/test_types.h \
+ /root/pygmy-dev/pygmy-sdk/HAL/inc/eoss3_hal_def.h \
+ /root/pygmy-dev/pygmy-sdk/Libraries/Utils/inc/common.h \
+ /root/pygmy-dev/pygmy-sdk/HAL/inc/s3x_clock_hal.h \
+ /root/pygmy-dev/pygmy-sdk/FreeRTOS/include/FreeRTOS.h \
+ /storage/emulated/0/github/vaman/arm/codes/blink/inc/FreeRTOSConfig.h \
+ /storage/emulated/0/github/vaman/arm/codes/blink/inc/Fw_global_config.h \
+ /root/pygmy-dev/pygmy-sdk/FreeRTOS/include/projdefs.h \
+ /root/pygmy-dev/pygmy-sdk/FreeRTOS/include/portable.h \
+ /root/pygmy-dev/pygmy-sdk/FreeRTOS/include/deprecated_definitions.h \
+ /root/pygmy-dev/pygmy-sdk/FreeRTOS/portable/GCC/ARM_CM4F_quicklogic_s3XX/portmacro.h \
+ /root/pygmy-dev/pygmy-sdk/FreeRTOS/include/mpu_wrappers.h \
+ /root/pygmy-dev/pygmy-sdk/FreeRTOS/include/task.h \
+ /root/pygmy-dev/pygmy-sdk/FreeRTOS/include/list.h \
+ /root/pygmy-dev/pygmy-sdk/HAL/inc/eoss3_dev.h
+
+/storage/emulated/0/github/vaman/arm/codes/blink/inc/Fw_global_config.h:
+
+/root/pygmy-dev/pygmy-sdk/HAL/inc/eoss3_dev.h:
+
+/root/pygmy-dev/pygmy-sdk/Libraries/CMSIS/inc/core_cm4.h:
+
+/root/pygmy-dev/pygmy-sdk/Libraries/CMSIS/inc/core_cmInstr.h:
+
+/root/pygmy-dev/pygmy-sdk/Libraries/CMSIS/inc/core_cmFunc.h:
+
+/root/pygmy-dev/pygmy-sdk/Libraries/CMSIS/inc/core_cm4_simd.h:
+
+/root/pygmy-dev/pygmy-sdk/HAL/inc/eoss3_hal_adc.h:
+
+/root/pygmy-dev/pygmy-sdk/HAL/inc/test_types.h:
+
+/root/pygmy-dev/pygmy-sdk/HAL/inc/eoss3_hal_def.h:
+
+/root/pygmy-dev/pygmy-sdk/Libraries/Utils/inc/common.h:
+
+/root/pygmy-dev/pygmy-sdk/HAL/inc/s3x_clock_hal.h:
+
+/root/pygmy-dev/pygmy-sdk/FreeRTOS/include/FreeRTOS.h:
+
+/storage/emulated/0/github/vaman/arm/codes/blink/inc/FreeRTOSConfig.h:
+
+/storage/emulated/0/github/vaman/arm/codes/blink/inc/Fw_global_config.h:
+
+/root/pygmy-dev/pygmy-sdk/FreeRTOS/include/projdefs.h:
+
+/root/pygmy-dev/pygmy-sdk/FreeRTOS/include/portable.h:
+
+/root/pygmy-dev/pygmy-sdk/FreeRTOS/include/deprecated_definitions.h:
+
+/root/pygmy-dev/pygmy-sdk/FreeRTOS/portable/GCC/ARM_CM4F_quicklogic_s3XX/portmacro.h:
+
+/root/pygmy-dev/pygmy-sdk/FreeRTOS/include/mpu_wrappers.h:
+
+/root/pygmy-dev/pygmy-sdk/FreeRTOS/include/task.h:
+
+/root/pygmy-dev/pygmy-sdk/FreeRTOS/include/list.h:
+
+/root/pygmy-dev/pygmy-sdk/HAL/inc/eoss3_dev.h:
