bd5b5f6f74bd62799163c69884c0bdd2c37c0f98
gadepall <gadepall@gmail.com>
2021-04-09 13:20:57 +0530

    Add existing files

--- arm/codes/sevenseg/loop/src/main.c
+++ arm/codes/sevenseg/loop/src/main.c
@@ -1,12 +1,12 @@
 /*==========================================================
 Code by G V V Sharma
-Feb 26, 2019,
+Apr 9, 2021,
 Released under GNU/GPL
 https://www.gnu.org/licenses/gpl-3.0.en.html
 /*==========================================================
  *
  *    File   : main.c
- *    Purpose: main for Pygmy Decade Counter 
+ *    Purpose: main for Pygmy Seven Segment Display Counter
  *                                                          
  *=========================================================*/
 
@@ -46,9 +46,17 @@ const char *SOFTWARE_VERSION_STR;
 
 extern void qf_hardwareSetup();
 static void nvic_init(void);
-void PyHal_Set_GPIO(uint8_t gpionum, uint8_t gpioval);
+#define GPIO_OUTPUT_MODE (1)
+#define GPIO_INPUT_MODE (0)
+void PyHal_GPIO_SetDir(uint8_t gpionum,uint8_t iomode);
+int PyHal_GPIO_GetDir(uint8_t gpionum);
+int PyHal_GPIO_Set(uint8_t gpionum, uint8_t gpioval);
+int PyHal_GPIO_Get(uint8_t gpionum);
+
+
+void sevenseg_setup(void);
 void sevenseg(int a, int b, int c, int d, int e, int f, int g);
-void disp(int num); 
+void disp(int num);
 
 int main(void)
 {
@@ -72,14 +80,19 @@ int main(void)
     CLI_start_task( my_main_menu );
 	HAL_Delay_Init();
 
+    sevenseg_setup();    //Sevenseg ready for display
     //Seven Segment GPIO
+  sevenseg(0,0,0,1,1,1,1);//The number 7
+    
+ //Seven Segment GPIO
 while(1){
-	disp(i);
+	disp(i); //display number
 	HAL_DelayUSec(1000000);    
 	i++;
 	if (i == 10)
 		i = 0;
 }
+
 
     /* Start the tasks and timer running. */
     vTaskStartScheduler();
@@ -103,15 +116,125 @@ void sevenseg(int a, int b, int c, int d, int e, int f, int g)
 
 {
     //Seven Segment GPIO
-    PyHal_Set_GPIO(4,a);//a
-    PyHal_Set_GPIO(5,b);//b
-    PyHal_Set_GPIO(6,c);//c
-    PyHal_Set_GPIO(7,d);//d
-    PyHal_Set_GPIO(8,e);//e
-    PyHal_Set_GPIO(10,f);//f
-    PyHal_Set_GPIO(11,g);//g
+    PyHal_GPIO_Set(4,a);//a
+    PyHal_GPIO_Set(5,b);//b
+    PyHal_GPIO_Set(6,c);//c
+    PyHal_GPIO_Set(7,d);//d
+    PyHal_GPIO_Set(8,e);//e
+    PyHal_GPIO_Set(10,f);//f
+    PyHal_GPIO_Set(11,g);//g
 }
 
+//needed for startup_EOSS3b.s asm file
+void SystemInit(void)
+{
+
+}
+
+//gpionum --> 0 --> 31 corresponding to the IO PADs
+//gpioval --> 0 or 1
+#define FGPIO_DIRECTION_REG (0x40024008)
+#define FGPIO_OUTPUT_REG (0x40024004)
+#define FGPIO_INPUT_REG (0x40024000)
+//Set GPIO(=gpionum) Mode: Input(iomode = 0) or Output(iomode = 1)
+//Before Set/Get GPIO value, the direction must be correctly set
+void PyHal_GPIO_SetDir(uint8_t gpionum,uint8_t iomode)
+{
+    uint32_t tempscratch32;
+
+    if (gpionum > 31)
+        return;
+
+    tempscratch32 = *(uint32_t*)(FGPIO_DIRECTION_REG);
+    if (iomode)
+        *(uint32_t*)(FGPIO_DIRECTION_REG) = tempscratch32 | (0x1 << gpionum);
+    else
+        *(uint32_t*)(FGPIO_DIRECTION_REG) = tempscratch32 & (~(0x1 << gpionum));
+
+}
+
+
+//Get current GPIO(=gpionum) Mode: Input(iomode = 0) or Output(iomode = 1)
+int PyHal_GPIO_GetDir(uint8_t gpionum)
+{
+    uint32_t tempscratch32;
+    int result = 0;
+
+    if (gpionum > 31)
+        return -1;
+
+    tempscratch32 = *(uint32_t*)(FGPIO_DIRECTION_REG);
+
+    result = ((tempscratch32 & (0x1 << gpionum)) ? GPIO_OUTPUT_MODE : GPIO_INPUT_MODE);
+
+    return result;
+}
+
+//Set GPIO(=gpionum) to 0 or 1 (= gpioval)
+//The direction must be set as Output for this GPIO already
+//Return value = 0, success OR -1 if error.
+int PyHal_GPIO_Set(uint8_t gpionum, uint8_t gpioval)
+{
+    uint32_t tempscratch32;
+
+    if (gpionum > 31)
+        return -1;
+
+    tempscratch32 = *(uint32_t*)(FGPIO_DIRECTION_REG);
+
+    //Setting Direction moved out as separate API, we will only check
+    //*(uint32_t*)(FGPIO_DIRECTION_REG) = tempscratch32 | (0x1 << gpionum);
+    if (!(tempscratch32 & (0x1 << gpionum)))
+    {
+        //Direction not Set to Output
+        return -1;
+    }
+    
+    tempscratch32 = *(uint32_t*)(FGPIO_OUTPUT_REG);
+
+    if(gpioval > 0)
+    {
+        *(uint32_t*)(FGPIO_OUTPUT_REG) = tempscratch32 | (0x1 << gpionum);
+    }
+    else
+    {
+        *(uint32_t*)(FGPIO_OUTPUT_REG) = tempscratch32 & ~(0x1 << gpionum);
+    }    
+
+    return 0;
+}
+//Get GPIO(=gpionum): 0 or 1 returned (or in erros -1)
+//The direction must be set as Input for this GPIO already
+int PyHal_GPIO_Get(uint8_t gpionum)
+{
+    uint32_t tempscratch32;
+    uint32_t gpioval_input;
+
+    if (gpionum > 31)
+        return -1;
+
+    tempscratch32 = *(uint32_t*)(FGPIO_INPUT_REG);
+    gpioval_input = (tempscratch32 >> gpionum) & 0x1;
+
+    return ((int)gpioval_input);
+}
+
+
+
+void sevenseg_setup(void)
+
+{
+
+    //Set GPIO direction
+    PyHal_GPIO_SetDir(4,1);
+    PyHal_GPIO_SetDir(5,1);
+    PyHal_GPIO_SetDir(6,1);
+    PyHal_GPIO_SetDir(7,1);    
+    PyHal_GPIO_SetDir(8,1);
+    PyHal_GPIO_SetDir(10,1);
+    PyHal_GPIO_SetDir(11,1);    
+
+}
 
 void disp(int num)
 {
@@ -152,42 +275,4 @@ switch(num)
 	break;
 }
 }
-
-//needed for startup_EOSS3b.s asm file
-void SystemInit(void)
-{
-
-}
-
-//gpionum --> 0 --> 31 corresponding to the IO PADs
-//gpioval --> 0 or 1
-#define FGPIO_DIRECTION_REG (0x40024008)
-#define FGPIO_OUTPUT_REG (0x40024004)
-
-void PyHal_Set_GPIO(uint8_t gpionum, uint8_t gpioval)
-{
-    uint32_t tempscratch32;
-
-    if (gpionum > 31)
-        return;
-
-    tempscratch32 = *(uint32_t*)(FGPIO_DIRECTION_REG);
-
-    *(uint32_t*)(FGPIO_DIRECTION_REG) = tempscratch32 | (0x1 << gpionum);
-
-    
-    tempscratch32 = *(uint32_t*)(FGPIO_OUTPUT_REG);
-
-    if(gpioval > 0)
-    {
-        *(uint32_t*)(FGPIO_OUTPUT_REG) = tempscratch32 | (0x1 << gpionum);
-    }
-    else
-    {
-        *(uint32_t*)(FGPIO_OUTPUT_REG) = tempscratch32 & ~(0x1 << gpionum);
-    }    
-
-    return;
-}
-
 
