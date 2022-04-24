72f4625d79220ec7feb9fc33380aa9eee5846567
gadepall <gadepall@gmail.com>
2022-04-24 11:33:06 +0530

    Add existing files

---
+++ esp32/setup/codes/ide/src/main.cpp
@@ -0,0 +1,21 @@
+//Code by GVV Sharma
+//April 22, 2022
+//https://www.gnu.org/licenses/gpl-3.0.en.html
+//
+#include <Arduino.h>
+
+
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(18, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(18, HIGH);   // turn the LED on (HIGH is the voltage leve
+delay(500);
+  digitalWrite(18, LOW);   // turn the LED on (HIGH is the voltage leve
+
+delay(500);
+
+}
